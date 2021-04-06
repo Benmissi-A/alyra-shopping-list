a953038cbfa25804ced619c892a4d22216f556bd
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-06 10:18:30 +0200

    push

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+// ./src/index.js
+import React from "react"
+import ReactDOM from "react-dom"
+import "bootstrap/dist/css/bootstrap.css"
+import "./index.css"
+/* ... */
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
